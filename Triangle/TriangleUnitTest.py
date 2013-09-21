14ca2a992afc6149f0afeba12bf6d529f3f21b45
khanhtrinh <trinhlk_55@vnu.edu.vn>
2013-09-21 08:36:16 +0700

    Thêm tam giác vuông cân
    Thay đổi biến từ int thành float

--- Triangle/TriangleUnitTest.py
+++ Triangle/TriangleUnitTest.py
@@ -4,47 +4,55 @@ Created on Sep 16, 2013
 @author: KHANHTRINH
 '''
 import unittest
+import math
 
 def typeOfTriangle(a, b, c):
-    if (0 < a and a < 2^32-1) and (0 < b and b < 2^32-1) and (0 < c and c < 2^32-1):
+    if (float(a)) and (float(b)) and (float(c)):
         if ((a + b) > c) and ((a + c) > b) and ((c + b) > a):
             if a == b and b == c and c == a:
-                print("Tam giac deu")
+                print"Tam giac deu\n"
                 return True
-            elif (a == b) and (b == c) and (c == a):
-                print("Tam giac can")
-                return True
+            elif (a == b) or (b == c) or (c == a):
+                if (math.sqrt(a*a + b*b) == c) or (math.sqrt(c*c + b*b) == b) or (math.sqrt(a*a + c*c) == b):
+                    print "Tam gia vuong can\n"
+                    return True
+                else:
+                    print"Tam giac can\n"
+                    return True
             elif (a*a + b*b == c*c) or (c*c + b*b == a*a) or (a*a + c*c == b*b):
-                print("Tam giac vuong")
+                print"Tam giac vuong\n"
                 return True
             else:
-                print("Tam giac binh thuong")
+                print"Tam giac binh thuong\n"
                 return True 
         else:
-            print("Khong phai tam giac")
+            print"Khong phai tam giac\n"
             return False
     else:
-        print("Gia tri dau vao khong phu hop") 
+        print"Gia tri dau vao khong phu hop\n" 
         return False
     
 class triangle(unittest.TestCase):
-    def testInputInvalid(self):
-        print("Test case 1:")
+    def testInputInvalid(self): # Test invalid
+        print "Test case: a = %r, b = %r, c = %r" % ("-1",2,0)
         self.assertFalse(typeOfTriangle("-1", 2, 0), "Khong dung chuan so nguyen")
-    def testInputValid(self):
-        print("\nTest case 2:")
+    def testInputValid(self):   # Test invalid
+        print "Test case: a = %r, b = %r, c = %r" % (1,4,5)
         self.assertFalse(typeOfTriangle(1, 4, 5), "Khong dung do dai canh")
-    def testTGVuong(self):
-        print("\nTest case 3:")
+    def testTGVuong(self):  # Tam giac vuong
+        print "Test case: a = %r, b = %r, c = %r" % (3,4,5)
         self.assertTrue(typeOfTriangle(3, 4, 5), "Khong phai tam giac vuong")
-    def testTGcan(self):
-        print("\nTest case 4:")
+    def testTGcan(self):    # Tam giac can
+        print "Test case: a = %r, b = %r, c = %r" % (2,2,1)
         self.assertTrue(typeOfTriangle(2, 2, 1), "Khong phai tam giac can")
-    def testTGdeu(self):
-        print("\nTest case 5:")
+    def testTGdeu(self):    # Tam giac deu
+        print "Test case: a = %r, b = %r, c = %r" % (2,2,2)
         self.assertTrue(typeOfTriangle(2, 2, 2), "Khong phai tam giac deu")
-    def testTGthuong(self):
-        print("\nTest case 6:")
+    def testTGvuongcan(self):   # Tam giac vuong can
+        print "Test case: a = %r, b = %r, c = %r" % (1,1,math.sqrt(2))
+        self.assertTrue(typeOfTriangle(1, 1, math.sqrt(2)), "Khong phai tam giac deu")
+    def testTGthuong(self):     # Tam giac thuong
+        print "Test case: a = %r, b = %r, c = %r" % (3, 5, 7)
         self.assertTrue(typeOfTriangle(3, 5, 7), "Khong phai tam giac thuong")
         
 def main():
